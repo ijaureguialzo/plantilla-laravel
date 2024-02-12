ded4b4822b0572cf11b9da317b75e7f4ee7169e1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-12 16:53:39 +0100

    No mostrar contenido en la sidebar a los invitados

--- laravel/resources/views/layouts/sidebar.blade.php
+++ laravel/resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 15rem;">
-    <ul class="nav nav-pills flex-column mb-auto">
-        @for ($i = 0; $i < 1; $i++)
+    @auth
+        <ul class="nav nav-pills flex-column mb-auto">
             <li class="text-uppercase pt-2 pb-1 fw-semibold"
                 style="font-size: .75em;">{{ __('Administrator') }}</li>
             <li class="nav-item">
@@ -30,77 +30,77 @@
                     </div>
                 </a>
             </li>
-        @endfor
-        <li class="text-uppercase pt-2 pb-1 fw-semibold"
-            style="font-size: .75em;">{{ __('User') }}</li>
-        <li class="nav-item">
-            <button
+            <li class="text-uppercase pt-2 pb-1 fw-semibold"
+                style="font-size: .75em;">{{ __('User') }}</li>
+            <li class="nav-item">
+                <button
                     class="btn btn-toggle dropdown-toggle px-3 nav-link collapsed text-light w-100 text-start hover-background"
                     data-bs-toggle="collapse" data-bs-target="#dash-collapse" aria-expanded="true">
-                <i class="bi bi-list me-1"></i>
-                <span>{{ __('Desplegable') }}</span>
-            </button>
-            <div class="collapse" id="dash-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-1 ms-4">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">
-                            <div class="d-flex">
-                                <span class="hover-link">{{ __('Dashboard') }}</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">
-                            <div class="d-flex">
-                                <span class="hover-link">{{ __('Home') }}</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <a href="#" class="nav-link text-light">
-                            <div class="d-flex">
-                                <span class="hover-link">{{ __('Dashboard') }}</span>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item mb-2">
-            <button
+                    <i class="bi bi-list me-1"></i>
+                    <span>{{ __('Desplegable') }}</span>
+                </button>
+                <div class="collapse" id="dash-collapse">
+                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-1 ms-4">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-light">
+                                <div class="d-flex">
+                                    <span class="hover-link">{{ __('Dashboard') }}</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-light">
+                                <div class="d-flex">
+                                    <span class="hover-link">{{ __('Home') }}</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item mb-2">
+                            <a href="#" class="nav-link text-light">
+                                <div class="d-flex">
+                                    <span class="hover-link">{{ __('Dashboard') }}</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item mb-2">
+                <button
                     class="btn btn-toggle dropdown-toggle px-3 nav-link collapsed text-light w-100 text-start hover-background-active"
                     data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                <i class="bi bi-rocket me-1"></i>
-                <span>{{ __('Desplegable 2') }}</span>
-            </button>
-            <div class="collapse" id="home-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-1 ms-4">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">
-                            <div class="d-flex">
-                                <i class="bi bi-clipboard me-2"></i>
-                                <span class="hover-link">{{ __('Dashboard') }}</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-light">
-                            <div class="d-flex">
-                                <i class="bi bi-house me-2"></i>
-                                <span class="hover-link">{{ __('Home') }}</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <a href="#" class="nav-link text-light">
-                            <div class="d-flex">
-                                <i class="bi bi-clipboard me-2"></i>
-                                <span class="hover-link">{{ __('Dashboard') }}</span>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-    </ul>
+                    <i class="bi bi-rocket me-1"></i>
+                    <span>{{ __('Desplegable 2') }}</span>
+                </button>
+                <div class="collapse" id="home-collapse">
+                    <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-1 ms-4">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-light">
+                                <div class="d-flex">
+                                    <i class="bi bi-clipboard me-2"></i>
+                                    <span class="hover-link">{{ __('Dashboard') }}</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-light">
+                                <div class="d-flex">
+                                    <i class="bi bi-house me-2"></i>
+                                    <span class="hover-link">{{ __('Home') }}</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item mb-2">
+                            <a href="#" class="nav-link text-light">
+                                <div class="d-flex">
+                                    <i class="bi bi-clipboard me-2"></i>
+                                    <span class="hover-link">{{ __('Dashboard') }}</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    @endauth
 </div>
